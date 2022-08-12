284283d44a702d908dc0e3c6e0b9008f89f8a7cc
lucky <lucky.hw.kim@gmail.com>
2022-08-11 19:51:19 -0700

    mutation front

--- index.js
+++ index.js
@@ -1,140 +1,110 @@
-const { ApolloServer, gql } = require('apollo-server')
+// Using apollo server
+const {v1: uuid} = require('uuid')
+const { ApolloServer, gql, UserInputError} = require('apollo-server');
 
-let authors = [
+let persons = [
   {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431"
   },
   {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
   },
   {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
-*/
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
   },
 ]
 
 const typeDefs = gql`
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks(author: String!): [BookTitle]
-    allAuthors: [AllAuthor]
-  }
+type Address {
+  street: String!
+  city: String! 
+}
 
-  type Book {
-    title: String
-    author: String!
-    published: Int!
-    genres: [String!]!
-    id: ID!
-  }
+type Person {
+  name: String!
+  phone: String
+  address: Address!
+  id: ID!
+}
 
-  type Author {
+type Mutation {
+  addPerson(
     name: String!
-    id: ID!
-    born: Int
-  }
+    phone: String
+    street: String!
+    city: String!
+  ): Person
+  editNumber(
+    name: String!
+    phone: String!
+  ): Person
+}
 
-  type AllAuthor {
-    bookCount: Int
-    name: String
-  }
+enum YesNo {
+  YES
+  NO
+}
 
-  type BookTitle {
-    title: String
-  }
-
-
+type Query {
+  personCount: Int!
+  allPersons(phone: YesNo): [Person!]!
+  findPerson(name: String!): Person
+}
 `
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args)=> {
-      const filteredBook = books.filter(b => b.author === args.author)
-      return filteredBook.map(b => b.title)
-    }
-      ,
-    allAuthors: () => authors.map(({ name }) => {
-      const bookCount = books.filter(b => b.author === name).length;
-      return { name, bookCount}
+    personCount: () => persons.length,
+    allPersons: (root, args) => {
+      if(!args.phone) {
+        return persons
+      }
+      const byPhone = person => 
+      args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
+    findPerson: (root, args) => 
+    persons.find(p => p.name === args.name)
+  },
+  Person: {
+    address: (root) => ({
+        street: root.street,
+        city: root.city
     })
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if(persons.find(person => person.name === args.name)){
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name,
+        })
+      }
+      const person = { ...args, id: uuid()}
+      persons.push(person)
+      return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if(!person) {
+        return null
+      }
+
+      const updatedPerson = { ...person, phone: args.phone }
+
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
+    }
   }
 }
 
@@ -143,6 +113,6 @@ const server = new ApolloServer({
   resolvers,
 })
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+server.listen().then(({url}) => {
+  console.log(`Server ready at ${url}`);
 })
---
+++ server/src/App.jsx
@@ -0,0 +1,44 @@
+import './App.css';
+import {gql, useQuery} from '@apollo/client'
+import Persons from './components/Persons';
+import { ALL_PERSONS } from './components/queries';
+import { useState } from 'react';
+import PersonForm from './components/PersonForm'
+
+function App() {
+  const [errorMessage, setErrorMessage] = useState(null)
+  const result = useQuery(ALL_PERSONS, {
+    pollInterval: 2000
+  })
+
+  if(result.loading) {
+    return <div>loading...</div>
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(()=>{
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div className="App">
+     <Notify errorMessage={errorMessage}/>
+     <Persons persons={result.data.allPersons}/>
+     <PersonForm setError={notify}/>
+    </div>
+  );
+}
+
+const Notify = ({errorMessage}) => {
+  if( !errorMessage) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
+export default App;
